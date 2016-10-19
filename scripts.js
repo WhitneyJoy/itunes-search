4dc517399283823a348d4970b097b2362fec43ec
Whitney <bdwseybold@gmail.com>
2016-10-19 07:55:11 -0500

    final touches

--- scripts.js
+++ scripts.js
@@ -1,30 +1,36 @@
+$('document').ready(function(){
+
 function getData(userInput){
-	$.ajax({
-    url: "https://itunes.apple.com/search?term="+ userInput,
+  $.ajax({
+    url: "https://itunes.apple.com/search?term=/"+userInput,
     type: "GET",
     dataType: 'jsonp'
   }).done(function(data){
-    console.log(data);
-
+    displayDataResults(data);
+    // console.log(data);
   }).fail(function(error){
-    console.log("an error occurred!");
+    // console.log("an error occurred!");
   });
 }
-$("#navigation").click(function(){
+
+function displayDataResults(data){
+  for(var i = 0; i < data.results.length; i++){
+  var apiResults = data.results[i];
+  document.getElementById("displayArea").innerHTML += "<hr>" + "Song Name: " + apiResults.trackName + " Artist: " + apiResults.artistName + " Album: " + apiResults.collectionName;
+}
+
+}
+
+
+function collectData(){
+  return document.getElementById("searchBar").value;
+}
+
+
+
+$("#searchArea").click(function(){
+  var userInput = collectData();
   getData(userInput);
 });
 
-function displayDataResults() {
-
-}
-
-// window.print();
-
-// $(function() {
-
-// 	$('cssSelector').method();
-// 	$
-//jquery codes goes in here
-
-// });
-// }
+});
