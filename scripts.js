0015a57b2db9be028c06023bfe6ec63e11b2e4a3
Whitney <bdwseybold@gmail.com>
2016-10-19 15:43:29 -0500

    gradient color change

--- scripts.js
+++ scripts.js
@@ -14,7 +14,7 @@ function getData(userInput){
 function displayDataResults(data){
   for(var i = 0; i < data.results.length; i++){
   var apiResults = data.results[i];
-  document.getElementById("displayArea").innerHTML += "<hr>" + "Song Name: " + apiResults.trackName + " Artist: " + apiResults.artistName + " Album: " + apiResults.collectionName + "<hr>" + "<img alt='album art' src=" + apiResults.artworkUrl60 + " height='42' width='42'>"  +  "<br>" + "<audio controls><source src=" + apiResults.previewUrl + " type='audio/mpeg'></audio>";
+  document.getElementById("displayArea").innerHTML += "<hr>" + "Song Name: " + apiResults.trackName + " Artist: " + apiResults.artistName + " Album: " + apiResults.collectionName + "<hr>" + "<img alt='album art' src=" + apiResults.artworkUrl100 + " height='150' width='150'>"  +  "<br>" + "<audio controls><source src=" + apiResults.previewUrl + " type='audio/mpeg'></audio>";
 }
 
 }
